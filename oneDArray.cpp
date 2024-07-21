b6d57edd8837c788ba4ec598fe2ae8594aebf25c
rajendra <rpancholi522@gmail.com>
2024-07-21 19:40:21 +0530

    1D and 2D array initialize and display element of an array using for loop.

---
+++ oneDArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // type 1: array initialization and declaration.
+    int arr[] = {11, 12, 13, 14, 15};
+
+    // type 2:
+    int arr2[5];
+    arr2[0] = 21;
+    arr2[1] = 22;
+    arr2[2] = 23;
+    arr2[3] = 24;
+    arr2[4] = 25;
+
+    arr2[4] = 222;
+    // Access the elements of an array.
+    int ln = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < ln; i++)
+    {
+        // cout << arr[i] << " ";
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
---
+++ twoDArray.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[4][4] = {
+        {11, 12, 13, 14},
+        {21, 22, 23, 24},
+        {31, 32, 33, 34},
+        {41, 42, 43, 44}};
+
+    arr[1][3] = 225;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
