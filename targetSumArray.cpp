8569789138514a830149ce03be15018ae7e19c9b
rajendra <rpancholi522@gmail.com>
2024-07-26 16:07:09 +0530

    solve Q2: Find the total number of triplets in the array whose sum is equal to the given value of x.

--- targetSumArray.cpp
+++ targetSumArray.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int arr1[] = {1, 2, 3, 0, 5, 6};
+    int arr1[] = {1, 2, 4, 5, 3, 6};
     int ln = sizeof(arr1) / sizeof(arr1[0]);
     int x;
     int count = 0;
@@ -11,15 +11,33 @@ int main()
     cin >> x;
 
     // Q1: Find the total number of pairs in the array whose sum is equal to the given value of x.
+    // for (int i = 0; i < ln; i++)
+    // {
+    //     for (int j = i + 1; j < ln; j++)
+    //     {
+    //         if (arr1[i] + arr1[j] == x)
+    //         {
+    //             count++;
+    //         }
+    //     }
+    // }
+    // cout << "Total number of pairs is: " << count << endl;
+
+    // Q2: Find the total number of triplets in the array whose sum is equal to the given value of x.
     for (int i = 0; i < ln; i++)
     {
         for (int j = i + 1; j < ln; j++)
         {
-            if (arr1[i] + arr1[j] == x)
+            for (int k = j + 1; k < ln; k++)
             {
-                count++;
+                if (arr1[i] + arr1[j] + arr1[k] == x)
+                {
+                    count++;
+                }
             }
         }
     }
-    cout << "Total number of pairs is: " << count << endl;
+    cout << "Total triplet is: " << count << endl;
+
+    return 0;
 }
