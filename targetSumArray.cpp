0171f155bb8e46dd25f340078ab8f7ed71101975
Rajendra Pancholi <rpancholi522@gmail.com>
2024-07-29 15:54:43 +0530

    Solve Q3: Find the unique number in given array where all the elements are being repeated twice with one value being unique.

--- targetSumArray.cpp
+++ targetSumArray.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int arr1[] = {1, 2, 4, 5, 3, 6};
-    int ln = sizeof(arr1) / sizeof(arr1[0]);
-    int x;
-    int count = 0;
-    cout << "Enter x: ";
-    cin >> x;
+    // int arr1[] = {1, 2, 4, 5, 3, 6};
+    // int ln = sizeof(arr1) / sizeof(arr1[0]);
+    // int x;
+    // int count = 0;
+    // cout << "Enter x: ";
+    // cin >> x;
 
     // Q1: Find the total number of pairs in the array whose sum is equal to the given value of x.
     // for (int i = 0; i < ln; i++)
@@ -38,6 +39,28 @@ int main()
     //     }
     // }
     // cout << "Total triplet is: " << count << endl;
+
+    // Q3: Find the unique number in given array where all the elements are being repeated twice with one value being unique.
+    int arr2[] = {1, 2, 3, 2, 3, 4, 1, 5};
+    int ln2 = sizeof(arr2) / sizeof(arr2[0]);
+    for (int i = 0; i < ln2; i++)
+    {
+        for (int j = i + 1; j < ln2; j++)
+        {
+            if (arr2[i] == arr2[j])
+            {
+                arr2[i] = arr2[j] = -1;
+            }
+        }
+    }
+    for (int i = 0; i < ln2; i++)
+    {
+        if (arr2[i] > 0)
+        {
+            cout << arr2[i] << " ";
+        }
+    }
+    cout << endl;
 
     return 0;
 }
