cc5c82053f975ebafc776ad29bf5b222d4430664
Rajendra Pancholi <rpancholi522@gmail.com>
2024-11-07 01:24:46 +0530

    optimize solution to caluclate the power of a number.

--- recursion/powerOfNumber.cpp
+++ recursion/powerOfNumber.cpp
@@ -6,8 +6,8 @@ Organization        : NIT Patna
 ***********************/
 #include <iostream>
 using namespace std;
-// Method 1: Time complexity is O(n)
-int pow_rec(int p, int q)
+// Method 1: Time complexity is O(q)
+/* int pow_rec(int p, int q)
 {
     // Base case
     if (q == 0)
@@ -15,8 +15,27 @@ int pow_rec(int p, int q)
         return 1;
     }
     return p * pow_rec(p, q - 1);
-}
+} */
 
+// Method 1: Time complexity is O(log(q))
+int pow_rec(int p, int q)
+{
+    // Base case of the recursion
+    if (q == 0)
+        return 1;
+    if (q % 2 == 0)
+    {
+        // If q is even
+        int res = pow_rec(p, q / 2);
+        return res * res;
+    }
+    else
+    {
+        // If q is odd
+        int res = pow_rec(p, (q - 1) / 2);
+        return p * res * res;
+    }
+}
 int main()
 {
     int p;
