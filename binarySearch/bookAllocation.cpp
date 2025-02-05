a6456b170ee65aa031bd9cae4afaef7a1f1913c2
RajendraPancholi <rpancholi522@gmail.com>
2025-02-05 23:08:17 +0530

    Implement books allocation solution using linear method.

---
+++ binarySearch/bookAllocation.cpp
@@ -0,0 +1,66 @@
+/**********************
+Program Name        : Book Allocation problem
+Author              : Rajendra Pancholi
+Date of creation    : 05/02/2025
+Organization        : NIT Patna
+***********************/
+/**********************
+ * Given an array 'arr' of interger numbers,'arr[i]' represents the number of pages in the 'i-th' book.
+ * Ther are 'm' number of students, and the task is to allocate all the books to athe students.
+ * Allocate books in such a way that.
+ * 1. Each student gets at least one book.
+ * 2. Each book should be allocated to to only one student.
+ * 3. Book allocation should be in a contiguous manner.
+ * Your have to allocate the book to 'm' students such that the maximum number of pages assigned to a student is minimum.
+ * If the allocation of books is not possible return -1.
+
+***********************/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// M1: Linear method
+int countStudents(vector<int> &arr, int pages)
+{
+    int student = 1, stdPages = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (stdPages + arr[i] <= pages)
+        {
+            stdPages += arr[i];
+        }
+        else
+        {
+            student++;
+            stdPages = arr[i];
+        }
+    }
+    return student;
+}
+
+int minPages(vector<int> &arr, int students)
+{
+    // count the total number of pages in all the books.
+    int maxPages = 0;
+    for (int i = 0; i < arr.size(); i++)
+        maxPages += arr[i];
+    // check for possibility
+    for (int pages = 0; pages <= maxPages; pages++)
+    {
+        int countStd = countStudents(arr, pages);
+        // cout << pages << endl;
+        if (countStd == students)
+            return pages;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {25, 46, 28, 49, 24};
+    int students = 4;
+    cout << "Minimum pages: " << minPages(arr, students) << endl;
+    return 0;
+}
