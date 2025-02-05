1316a19db150a7bd3170f42171bcf3bec87a931f
RajendraPancholi <rpancholi522@gmail.com>
2025-02-05 23:29:06 +0530

    Optimized method (binary search) [TC = O(sizzOfArr*log(maxPg))]

--- binarySearch/bookAllocation.cpp
+++ binarySearch/bookAllocation.cpp
@@ -21,7 +21,7 @@ Organization        : NIT Patna
 
 using namespace std;
 
-// M1: Linear method
+// M1: Linear method [TC = O(maxPages*sizzOfArr)]
 int countStudents(vector<int> &arr, int pages)
 {
     int student = 1, stdPages = 0;
@@ -57,10 +57,38 @@ int minPages(vector<int> &arr, int students)
     return -1;
 }
 
+// Optimized method (binary method) [TC = O(sizzOfArr*log(maxPg))]
+int minPagesM2(vector<int> &arr, int students)
+{
+    // Count the total number of pages in all the books.
+    int right = 0, left = arr[0];
+    int minPg = -1;
+    for (int i = 0; i < arr.size(); i++)
+        right += arr[i];
+    // Check for possibility
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (countStudents(arr, mid) <= students)
+        {
+            minPg = mid;
+            // jump to left side
+            right = mid - 1;
+        }
+        else
+        {
+            // jump to right side
+            left = mid + 1;
+        }
+    }
+
+    return minPg;
+}
 int main()
 {
     vector<int> arr = {25, 46, 28, 49, 24};
     int students = 4;
-    cout << "Minimum pages: " << minPages(arr, students) << endl;
+    // cout << "Minimum pages: " << minPages(arr, students) << endl;
+    cout << "Minimum pages: " << minPagesM2(arr, students) << endl;
     return 0;
 }
