f3106180fa6bdac539b13385181f1d463b7d38cd
RajendraPancholi <rpancholi522@gmail.com>
2025-08-19 13:14:22 +0530

    correction in mergeSort argument, and for loop of merge funtion.

--- sortingAlgo/mergeSort.cpp
+++ sortingAlgo/mergeSort.cpp
@@ -36,7 +36,7 @@ void merge(vector<int> &arr, int low, int mid, int high)
         temp.push_back(arr[right]);
         right++;
     }
-    for (int i = low; i < temp.size(); i++)
+    for (int i = low; i <= high; i++)
     {
         arr[i] = temp[i - low];
     }
@@ -59,7 +59,7 @@ int main()
     // vector<int> arr = {12, 11, 13, 5, 6, 7};
     vector<int> arr = {38, 27, 43, 10};
     int n = arr.size();
-    mergeSort(arr, 0, n);
+    mergeSort(arr, 0, n-1);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
