dc8651bfb859b1eb78922ef2e7c7271493f6cb2d
Rajendra Pancholi <rpancholi522@gmail.com>
2026-04-25 22:30:03 +0530

    two strings str1 and str2, find the length of their longest common substring.

--- dp/commonSubsequence.cpp
+++ dp/commonSubsequence.cpp
@@ -24,14 +24,17 @@ public:
     int m = text2.size();
 
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
+    
+    // int ans = 0;
     for (int i = 1; i <= n; i++){
       for (int j = 1; j <= m; j++){
         if (text1[i - 1] == text2[j - 1]){
           dp[i][j] = 1 + dp[i - 1][j - 1];
+          // ans = max(ans, dp[i][j]);
         }
         else{
           dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+          // dp[i][j] = 0;
         }
       }
     }
@@ -61,6 +64,6 @@ int main() {
   string s1 = "abcde";
   string s2 = "ace"; Solution sol;
   cout << "LCS: " << sol.longestCommonSubsequence(s1, s2) << endl;
-
+  cout<<"LCS lenght: " << sol.longestCommonSubsequence(s1, s2).size()<<endl;
   return 0;
 }
