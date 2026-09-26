61e5f5fb7c9c61002f716c1d4e9db45437598f47
Raje Pancholi <rpancholi522@gmail.com>
2026-09-26 19:19:04 +0530

    findKthPositive

---
+++ binarySearch/findKthPositive.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int findKthPositive(vector<int> &arr, int k) {
+    int l = 0, r = arr.size() - 1;
+    while (l <= r) {
+      int mid = l + (r - l) / 2;
+      int missing = arr[mid] - (mid + 1);
+      if (missing < k)
+        l = mid + 1;
+      else
+        r = mid - 1;
+    }
+    return l + k;
+  }
+};
